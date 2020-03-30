38148320e1cbd0c4a2a71025631ed93e89578cb4
williampcastro <willianpcastro@hotmail.com>
2020-03-30 18:03:48 -0300

    :construction: add controllers

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,23 @@
+const connectionDb = require('../database/connection');
+const crypto = require('crypto');
+
+module.exports = {
+    async index(request, response) {
+        const ongs = await connectionDb('ongs').select('*');
+        return response.json(ongs);
+    },
+
+    async create(request, response) {
+        const { name, email, whatsapp, city, uf } = request.body;
+        const id = crypto.randomBytes(4).toString('HEX');
+        await connectionDb('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        })
+        return response.json({ id });
+    }
+};
