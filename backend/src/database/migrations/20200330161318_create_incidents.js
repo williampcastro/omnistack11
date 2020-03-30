8eac90226456c2396924bfb4b68bd0a0b6683a8e
williampcastro <willianpcastro@hotmail.com>
2020-03-30 18:03:31 -0300

    :construction: database configuration

---
+++ backend/src/database/migrations/20200330161318_create_incidents.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('incidents', function (table){
+        table.increments();
+        
+        table.string('title').notNullable();
+        table.string('description').notNullable();
+        table.decimal('value').notNullable();
+        
+        // foreign key
+        table.string('ong_id').notNullable();
+        
+        // relation of foreing key
+        table.foreign('ong_id').references('id').inTable('ongs')
+    })
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('incidents');
+  };
+  
