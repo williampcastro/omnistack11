ab260032f457062354dfaffb4312838d6221b7c7
williampcastro <willianpcastro@hotmail.com>
2020-03-30 18:02:03 -0300

    :construction: implements index and routes

---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const OnGController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create)
+
+routes.get('/ongs', OnGController.index);
+routes.post('/ongs', OnGController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
