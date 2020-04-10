13b569df136f1c42e4dd58218d548560fe8ab0ba
williampcastro <willianpcastro@hotmail.com>
2020-04-10 10:40:09 -0300

    :construction: structure of project

---
+++ mobile/src/pages/incidents/index.js
@@ -0,0 +1,9 @@
+import React from './node_modules/react';
+import { View } from 'react-native';
+import styles from './styles';
+
+export default function incidents() {
+  return (
+    <View />
+  );
+}
