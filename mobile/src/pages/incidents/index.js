5bc6441b71c887e25eff9dab90526de4b2945efa
williampcastro <willianpcastro@hotmail.com>
2020-04-10 16:49:25 -0300

    :construction::iphone: add scroll

--- mobile/src/pages/incidents/index.js
+++ mobile/src/pages/incidents/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, Image, Text, Button, TouchableOpacity } from 'react-native';
+import { View, FlatList, Image, Text, Button, TouchableOpacity } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
@@ -18,70 +18,34 @@ export default function Incidents() {
             <Text style={styles.title}>Bem-vindo!</Text>
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
 
-            <View style={styles.incidentsList}>
-                <View style={styles.incident}>
+            <FlatList
+                style={styles.incidentsList}
+                data={[1, 2, 3]}
+                keyExtractor={incident => String(incident)}
+                showsVerticalScrollIndicator={false}
+                renderItem={() => (
+                    <View style={styles.incident}>
 
-                    <Text style={styles.incidentProperty}>ONG: </Text>
-                    <Text style={styles.incidentValue}>APAD</Text>
+                        <Text style={styles.incidentProperty}>ONG: </Text>
+                        <Text style={styles.incidentValue}>APAD</Text>
 
-                    <Text style={styles.incidentProperty}>Caso: </Text>
-                    <Text style={styles.incidentValue}>Cadela atropelada</Text>
+                        <Text style={styles.incidentProperty}>Caso: </Text>
+                        <Text style={styles.incidentValue}>Cadela atropelada</Text>
 
-                    <Text style={styles.incidentProperty}>Valor: </Text>
-                    <Text style={styles.incidentValue}>R$ 120,00</Text>
+                        <Text style={styles.incidentProperty}>Valor: </Text>
+                        <Text style={styles.incidentValue}>R$ 120,00</Text>
 
-                    <TouchableOpacity
-                        style={styles.detailsButton}
-                        onPress={() => { }}
-                    >
-                        <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-                        <Feather name="arrow-right" size={16} color="#e02041"></Feather>
-                    </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.detailsButton}
+                            onPress={() => { }}
+                        >
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Feather name="arrow-right" size={16} color="#e02041"></Feather>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
 
-                </View>
-
-                <View style={styles.incident}>
-
-                    <Text style={styles.incidentProperty}>ONG: </Text>
-                    <Text style={styles.incidentValue}>APAD</Text>
-
-                    <Text style={styles.incidentProperty}>Caso: </Text>
-                    <Text style={styles.incidentValue}>Cadela atropelada</Text>
-
-                    <Text style={styles.incidentProperty}>Valor: </Text>
-                    <Text style={styles.incidentValue}>R$ 120,00</Text>
-
-                    <TouchableOpacity
-                        style={styles.detailsButton}
-                        onPress={() => { }}
-                    >
-                        <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-                        <Feather name="arrow-right" size={16} color="#e02041"></Feather>
-                    </TouchableOpacity>
-
-                </View>
-
-                <View style={styles.incident}>
-
-                    <Text style={styles.incidentProperty}>ONG: </Text>
-                    <Text style={styles.incidentValue}>APAD</Text>
-
-                    <Text style={styles.incidentProperty}>Caso: </Text>
-                    <Text style={styles.incidentValue}>Cadela atropelada</Text>
-
-                    <Text style={styles.incidentProperty}>Valor: </Text>
-                    <Text style={styles.incidentValue}>R$ 120,00</Text>
-
-                    <TouchableOpacity
-                        style={styles.detailsButton}
-                        onPress={() => { }}
-                    >
-                        <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-                        <Feather name="arrow-right" size={16} color="#e02041"></Feather>
-                    </TouchableOpacity>
-
-                </View>
-            </View>
         </View>
     );
 }
