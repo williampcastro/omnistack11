e6dd2bd34f7f04cfab5512b63e141bfb38fb199d
williampcastro <willianpcastro@hotmail.com>
2020-04-22 08:55:50 -0300

    :sparkles: add validators on GET Profile route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -26,6 +26,10 @@ routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown()
+}), ProfileController.index);
 
 module.exports = routes;
