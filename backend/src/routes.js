5252c3908d8f3de51a41d6bdd801b8efbedf5358
williampcastro <willianpcastro@hotmail.com>
2020-04-22 09:42:59 -0300

    :sparkles: add validators on CREATE and GET Incidents routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -22,8 +22,22 @@ routes.post('/ongs', celebrate({
     })
 }), OnGController.create);
 
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    }).unknown(),
+}),IncidentController.index);
+
+routes.post('/incidents', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required().min(1),
+    }),
+}), IncidentController.create);
 
 routes.delete('/incidents/:id', celebrate({
     [Segments.PARAMS]: Joi.object().keys({
